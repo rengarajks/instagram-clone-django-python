4e07463b45ed5378370c1dac96747df6a9907d62
tmachine <tmachine@sys10.PEC.IN>
2024-02-02 08:44:33 +0530

    base

---
+++ instaclone/posts/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+
+from django.contrib.auth.models import User
+
+
+from django.utils.text import slugify
+import uuid
+from django.urls import reverse
+
+def user_directory_path(instance,filename):
+    return 'user_{0}/{1}'.format(instance.user.id,filename)
+
+
+class Tag(models.Model):
+    title=models.CharField(max_length=100,verbose_name='Tag')
+    slug=models.SlugField(null=False,default=uuid.uuid1,unique=True)
+
+    class Meta:
+        verbose_name = 'Tag'
+        verbose_name_plural = 'Tags'
+
+    def get_absolute_url(self):
+        return reverse('tags', args=[self.slug])
+    
+    def __str__(self):
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
+    
+
+
+
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    picture = models.ImageField(upload_to=user_directory_path, verbose_name="Picture")
+    caption = models.CharField(max_length=10000, verbose_name="Caption")
+    posted = models.DateField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag, related_name="tags")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.IntegerField(default=0)
+
+    def get_absolute_url(self):
+        return reverse("post-details", args=[str(self.id)])
