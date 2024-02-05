f1d73800d57a3499c5bd3ea4ea18c1441f0ea229
tmachine <tmachine@sys08.PEC.IN>
2024-02-05 11:11:25 +0530

    follow function updated

--- instaclone/instaclone/urls.py
+++ instaclone/instaclone/urls.py
@@ -19,14 +19,16 @@ from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
 
-from userauths.views import UserProfile
+from userauths.views import UserProfile,follow
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('posts.urls')),
     path('<username>/',UserProfile,name="profile"),
-    path('<username>/saved',UserProfile,name="saved")
+    path('<username>/saved',UserProfile,name="saved"),
+    path('<username>/follow/<option>/',follow,name="follow"),
+
 ]
 if settings.DEBUG:
     urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
--- instaclone/userauths/views.py
+++ instaclone/userauths/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render,get_object_or_404
 from django.core.paginator import Paginator
 from django.urls import resolve
 from django.contrib.auth.models import User
+from django.db import transaction
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Profile 
-from posts.models import Post
+from posts.models import Post,Follow,Stream
+
 
 
 
@@ -20,6 +24,15 @@ def UserProfile(request, username):
     else:
         posts = profile.favourite.all()
    
+
+
+     # Profile Stats
+    posts_count = Post.objects.filter(user=user).count()
+    following_count = Follow.objects.filter(follower=user).count()
+    followers_count = Follow.objects.filter(following=user).count()
+
+    follow_status = Follow.objects.filter(following=user, follower=request.user).exists()
+
     # pagination
     paginator = Paginator(posts, 3)
     page_number = request.GET.get('page')
@@ -30,8 +43,39 @@ def UserProfile(request, username):
         'posts_paginator':posts_paginator,
         'profile':profile,
         'posts':posts,
-        'url_name':url_name
+        'url_name':url_name,
+
+        'posts_count':posts_count,
+        'following_count':following_count,
+        'followers_count':followers_count,
+        'follow_status':follow_status
+
         
        
     }
     return render(request, 'profile.html', context)
+
+
+
+
+
+def follow(request, username, option):
+    user = request.user
+    following = get_object_or_404(User, username=username)
+
+    try:
+        f, created = Follow.objects.get_or_create(follower=request.user, following=following)
+
+        if int(option) == 0:
+            f.delete()
+            Stream.objects.filter(following=following, user=request.user).all().delete()
+        else:
+            posts = Post.objects.all().filter(user=following)[:25]
+            with transaction.atomic():
+                for post in posts:
+                    stream = Stream(post=post, user=request.user, date=post.posted, following=following)
+                    stream.save()
+        return HttpResponseRedirect(reverse('profile', args=[username]))
+
+    except User.DoesNotExist:
+        return HttpResponseRedirect(reverse('profile', args=[username]))
